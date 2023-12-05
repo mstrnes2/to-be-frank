2512f95a758762f4b80cf7b89a38fb5f8cb12167
Erin Hamrick <erin.hamrick76@gmail.com>
2023-12-05 12:18:56 -0600

    Rendered quiz component on quiz page. started styling

---
+++ client/src/pages/QuizPage.jsx
@@ -0,0 +1,14 @@
+// QuizPage.js
+import React from 'react';
+import QuizComponent from '../components/Quiz';
+
+const QuizPage = () => {
+  return (
+    <div>
+      <h1></h1>
+      <QuizComponent />
+    </div>
+  );
+};
+
+export default QuizPage;
