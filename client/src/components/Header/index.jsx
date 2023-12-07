22ee4a59339052d76ad03c21d44f81e250755ab8
Molly Starnes <mstrnes2@gmail.com>
2023-12-07 17:50:45 -0600

    added header component

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -6,15 +6,17 @@ const Header = () => {
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
+    window.location.assign('/LoginPage');
   };
+
   return (
     <header >
       <div  >
          <div>
           {Auth.loggedIn() ? (
             <>
-              <Link className="btn btn-lg btn-info m-2" to="/me">
-                {Auth.getProfile().data.username}'s profile
+              <Link className="btn btn-lg btn-info m-2" to="/profiles/:username">
+                {Auth.getProfile().data.username}
               </Link>
               <button className="btn btn-lg btn-light m-2" onClick={logout}>
                 Logout
@@ -22,12 +24,6 @@ const Header = () => {
             </>
           ) : (
             <>
-              <Link className="login" to="/login">
-                Login
-              </Link>              
-              <Link className="login" to="/signup">
-                Signup
-              </Link>
             </>
           )}
         </div>
--- client/src/pages/QuizPage.jsx
+++ client/src/pages/QuizPage.jsx
@@ -1,12 +1,14 @@
 // QuizPage.js
 import React from 'react';
 import QuizComponent from '../components/Quiz';
+import Header from '../components/Header';
 
 
 const QuizPage = () => {
 
   return (
     <div>
+      <Header />
       <h1></h1>
       <QuizComponent />
     </div>
