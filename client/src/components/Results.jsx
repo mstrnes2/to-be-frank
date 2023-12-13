84879099bc725a1e50da6c60da4dfb30afe7c479
Erin Hamrick <erin.hamrick76@gmail.com>
2023-12-13 17:32:25 -0600

    restyled quiz component and results components in media queries

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -1,33 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 
-
-
 export default function Results({ me }) {
-    console.log('me:', me)
-    const { quizResults } = me.profile
-    console.log(quizResults.date)
-    return (
-        <div className='clickhere-resultscontainer'>
-            <div className='clickhere'>
-                Click here to order!
-            </div>
-            <div className='results-container'>
-                <div className='result-date-and-restaurant-name'>
-                    <div className='results-date'>
-                        {dayjs.unix(quizResults.date / 1000).format('MM/DD/YYYY h:mm A')}
-                    </div>
-                    <div className='restaurant-name'>
-                        {quizResults.restaurant}
-                    </div>
-                </div>
-                <figure>
-                    <a href={quizResults.link} title={quizResults.link}>
-                        <img src={quizResults.restaurantImage} alt={quizResults.restaurant} className='restaurant-image' />
-                    </a>
-                </figure>
-            </div>
-        </div>
-    );
-
+	console.log('me:', me);
+	const { quizResults } = me.profile;
+	console.log(quizResults.date);
+	return (
+		<div className='clickhere-resultscontainer'>
+			<div className='results-container'>
+				<div className='result-date-and-restaurant-name'>
+					<div className='results-date'>
+						{dayjs
+							.unix(quizResults.date / 1000)
+							.format('MM/DD/YYYY h:mm A')}
+					</div>
+					<div className='restaurant-name'>{quizResults.restaurant}</div>
+				</div>
+				<figure>
+					<a
+						href={quizResults.link}
+						title={quizResults.link}>
+						<img
+							src={quizResults.restaurantImage}
+							alt={quizResults.restaurant}
+							className='restaurant-image'
+						/>
+					</a>
+					<figcaption className='clickhere'>
+						Click here to order!
+					</figcaption>
+				</figure>
+			</div>
+		</div>
+	);
 }
